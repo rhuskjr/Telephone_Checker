3a7fcdbe1bd63b22cabc4e494bf087a1ad78f7ac
rhuskjr@gmail.com <rhuskjr@gmail.com>
2022-03-24 05:23:17 -0500

    rev2

--- Telephone_Checker.js
+++ Telephone_Checker.js
@@ -1,28 +1,29 @@
 function telephoneCheck(str) {
-//create string without spaces
-let newString = str.trim();
-//console.log(newString);
-let value = newString.match(/[^1-9-\(\)]/g)
-//console.log(value);
-if (!value){
-  value = newString.match(/[^\(\)-]/g);
-  if (value.length === 10){
-    console.log(true);
-    return true;
-  } else if (value.length === 11 && value[0] === 1){ 
-    console.log(true);
-    return true 
+  //create string without spaces
+  let newString = str.replace(/\s/g, '');
+  console.log(newString);
+  let value = newString.match(/[^1-9-\(\)]/g)
+  console.log(value);
+  if (!value) {
+    value = newString.match(/[^\(\)-]/g).join((''));
+    console.log(value);
+    console.log(value.length);
+    if (value.length === 10) {
+      console.log(true);
+      return true;
+    } else if (value.length === 11 && value[0] === '1') {
+      console.log(true);
+      return true
+    }
+    console.log(false);
+    return false;
   }
   console.log(false);
   return false;
 }
-console.log(false);
-  return false;
-}
-  const node = document.createElement('div');
-  node.textContent = "Hello World";
-  document.getElementById("tele_number").appendChild(node);
+
 //telephoneCheck("555-555-5555");
 //telephoneCheck("(555)555-5555");
 //telephoneCheck("5555555");
 telephoneCheck("1 555)555-5555");
+telephoneCheck("1 (555) 555-5555")
