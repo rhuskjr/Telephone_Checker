ca29d3355d995d9c7b78fa9c3ff280b3f582b641
rhuskjr@gmail.com <rhuskjr@gmail.com>
2022-03-23 21:31:24 -0500

    version 1

--- Telephone_Checker.js
+++ Telephone_Checker.js
@@ -1,5 +1,28 @@
 function telephoneCheck(str) {
-  return true;
+//create string without spaces
+let newString = str.trim();
+//console.log(newString);
+let value = newString.match(/[^1-9-\(\)]/g)
+//console.log(value);
+if (!value){
+  value = newString.match(/[^\(\)-]/g);
+  if (value.length === 10){
+    console.log(true);
+    return true;
+  } else if (value.length === 11 && value[0] === 1){ 
+    console.log(true);
+    return true 
+  }
+  console.log(false);
+  return false;
 }
-
-telephoneCheck("555-555-5555");
+console.log(false);
+  return false;
+}
+  const node = document.createElement('div');
+  node.textContent = "Hello World";
+  document.getElementById("tele_number").appendChild(node);
+//telephoneCheck("555-555-5555");
+//telephoneCheck("(555)555-5555");
+//telephoneCheck("5555555");
+telephoneCheck("1 555)555-5555");
